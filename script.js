04bc3fece2fba97ed2486a47b9bd9609b51da3f4
navvkaur <71594057+navvkaur@users.noreply.github.com>
2022-12-29 17:11:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,48 @@
 const http = require('http');
 const fs = require('fs');
 
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
-//app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/login',(req,res,next)=>{
 
     
         
-        res.write('<html>');
-        res.write('<head><title>Group Chat Application </title></head>');
-        res.write(`<form onsubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/message" method="POST"><input id="username" type="text" name"title">
-         <button type="submit">add</button></form>`);
-        res.write('</html>');  
+    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/message" method="GET"><input type="text" id="username" name="username" placeholder="Enter Username"><button type="submit"> Send </button></form>');
         
 
     });
-    app.use('/message',(req,res,next)=>{
-
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write(`<body><form onsubmit="localStorage.setItem('username', document.getElementById('username').value=localStorage.getItem('username'))" action="/message" method="POST">
-        <input type = "text" id = "message" name = "message">
-        <input type = "hidden" id = "username" name = "username">
-        <button type = "submit">SEND</button> </form></body>`);
-         res.write('</html>')
-         
-        
-
-    });
-
-     app.post('/message',(req,res,next)=>{
-       console.log(req.body.username);
-       console.log(req.body.message);
-       fs.writeFile("message.txt",`${req.body.username}:${req.body.message}`,{flag:'a'},(err)=>{
-        err ? console.log(err):res.redirect('/message');
-     });
-    });
-
-    app.get('/message',(req,res,next)=>{
+app.get('/message',(req,res,next)=>{
         fs.readFile('message.txt',(err,data)=>{
+
             if(err)
             {
                 console.log(err)
             }
-            res.send(`${data}form onsubmit="localStorage.setItem('username', document.getElementById('username').value=localStorage.getItem('username'))" action="/message" method="POST">
-            <input type = "text" id = "message" name = "message">
-            <input type = "hidden" id = "username" name = "username">
-            <button type = "submit">SEND</button> </form></body>`);
+            res.send(`${data}<form action="/" method = "POST" onSubmit = "document.getElementById('username').value = localStorage.getItem('username')" >
+            <input type="text" name="message" id = "message" placeholder="Enter Your Message">
+            <input type="hidden" id="username" name="username" placeholder="Enter Username">
+            <br>
+            <button type="submit"> Send </button></form>`);
         });
         
-    })
+     
+
+    });
+
+app.post('/',(req,res,next)=>{
+    console.log(req.body.username);
+        console.log(req.body.message);
+   
+        fs.writeFile('message.txt',`${req.body.username}:${req.body.message}`,{flag:'a'},(err)=>{
+            err ? console.log(err):res.redirect('/message');
+         });
+   
+       
+    });
+
+    
 
 
 
@@ -64,4 +55,5 @@ app.use('/login',(req,res,next)=>{
 
 
 app.listen(3000);
+
 
